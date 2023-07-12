69d8482a30e3a4e9b0af56a7f2ddfb97fa4b91f3
fchaconb <fchaconb@ucenfotec.ac.cr>
2023-07-11 23:30:25 -0600

    cambios landing y header

---
+++ JS/login.js
@@ -0,0 +1,4 @@
+function redirigirCuentaUsuarioGeneral() {
+    /* Esto es para cambiar llevarnos a donde sería el landing page de la cuenta de colaborador*/
+    window.location.href = "./landingUsuarioFinal.html";
+}
