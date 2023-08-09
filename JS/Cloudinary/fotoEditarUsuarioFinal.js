566f772bc2aff668b76fa83e74967e4868cb5c45
Rommel2023 <rrodriguezmo@ucenfotec.ac.cr>
2023-08-09 17:18:45 -0600

    IMAGEN EDITAR USUARIO FINAL

---
+++ JS/Cloudinary/fotoEditarUsuarioFinal.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    const boton = document.getElementById("botonFoto");
+    const imgElement = document.getElementById("fotoUsuario");
+
+    let myWidget = cloudinary.createUploadWidget(
+        {
+            cloudName: "dobj7jqwu",
+            uploadPreset: "preset.Rom",
+            clientAllowedFormats: ["jpg", "png", "jpeg"],
+            maxFileSize: 3000000,
+        },
+        (error, result) => {
+            if (!error && result && result.event === "success") {
+                imgElement.src = result.info.secure_url;
+                console.log("Done! Here is the image info: ", result.info);
+            }
+        }
+    );
+
+    boton.addEventListener(
+        "click",
+        function () {
+            myWidget.open();
+        },
+        false
+    );
+};
