607a7a289e7f86f3353e178365a2773ce663e00c
fchaconb <fchaconb@ucenfotec.ac.cr>
2023-08-09 19:51:17 -0600

    Revert "Imagenesss"
    This reverts commit 384929333ed950120fbfd7373f67f92f224bacc3.

--- JS/servicioImagen.js
+++ JS/servicioImagen.js
@@ -1,24 +1,19 @@
 window.onload = function () {
-    const buttonElement = document.getElementById("botonFoto");
-    const imgElement = document.getElementById("fotoUsuario");
+    const boton = document.getElementById("botonFoto");
 
     let myWidget = cloudinary.createUploadWidget(
         {
             cloudName: "dobj7jqwu",
             uploadPreset: "preset.Rom",
-            clientAllowedFormats: ["jpg", "png", "jpeg"],
-            maxFileSize: 3000000,
         },
         (error, result) => {
-
             if (!error && result && result.event === "success") {
-                imgElement.src = result.info.secure_url;
                 console.log("Done! Here is the image info: ", result.info);
             }
         }
     );
 
-    buttonElement.addEventListener(
+    boton.addEventListener(
         "click",
         function () {
             myWidget.open();
